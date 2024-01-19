bbf70b9ebcfaac701adcb077b358c685445ee389
Cesar <kaesar84@icloud.com>
2024-01-19 16:24:09 +0100

    Update

--- 01_Reading/15_alias.sql
+++ 01_Reading/15_alias.sql
@@ -4,7 +4,7 @@ Lección 10.9: https://youtu.be/OuJerKzV5T0?t=8667
 */
 
 -- Establece el alias 'Fecha de inicio en programación' a la columna init_date
-SELECT name, init_date AS 'Fecha de inicio en programación' FROM users WHERE name = 'Brais'
+SELECT name, init_date AS 'Fecha de inicio en programación' FROM users WHERE name = 'César'
 
 -- Consulta igual que la anterior. Representa la posibilidad de usar comillas dobles para cadenas
-SELECT name, init_date AS "Fecha de inicio en programación" FROM users WHERE name = "Brais"
+SELECT name, init_date AS "Fecha de inicio en programación" FROM users WHERE name = "César"
